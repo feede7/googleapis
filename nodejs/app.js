d404bc99a0370d9ede2b93e0cdd2a4f4e62f2d66
Fede P53s <agogantter>
2024-05-10 22:12:50 -0300

    First commit

---
+++ nodejs/app.js
@@ -0,0 +1,19 @@
+
+// Own fuctions
+const authenticate = require('./scripts/googleapi.authenticate')
+const calendar = require('./scripts/googleapi.calendar')
+const people = require('./scripts/googleapi.people')
+
+const main = async () => {
+    console.log('holis')
+
+    await authenticate.authorize().then(calendar.listEvents).catch(console.error);
+    await authenticate.authorize().then(people.listConnectionNames).catch(console.error);
+
+    console.log('arranca')
+    console.log(calendar.event_list)
+    console.log(people.contact_dict)
+
+}
+
+main()
